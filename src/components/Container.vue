d034bdb3635552feafc011819754c779c46c41bd
Abhirham Savarap <abhirham@br.iq>
2022-03-17 02:07:09 -0400

    WIP

---
+++ src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="Container">
+        <v-row justify="center">
+            <ImageCard />
+            <ImageCard />
+            <ImageCard />
+            <ImageCard />
+            <ImageCard />
+            <ImageCard />
+            <ImageCard />
+            <ImageCard />
+        </v-row>
+    </div>
+</template>
+
+<script>
+import ImageCard from "./ImageCard.vue";
+
+export default {
+    name: "Container",
+    components: {
+        ImageCard,
+    },
+};
+</script>
